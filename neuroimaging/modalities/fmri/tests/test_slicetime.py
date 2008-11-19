faa0aa83f8d3954a7ebc7df9413bca9b0cc39923
Jonathan Taylor <jonathan.taylor@stanford.edu>
2008-11-19 03:07:07 -0500

    fixed linear interpolation test

--- neuroimaging/modalities/fmri/tests/test_slicetime.py
+++ neuroimaging/modalities/fmri/tests/test_slicetime.py
@@ -19,18 +19,20 @@ def test_nointerp_sinc():
     Y = np.random.standard_normal(t.shape + (11,4,5))
     Y = Image(Y, CoordinateMap.identity(['t','z','y','x'], Y.shape))
     f = fromimage(Y, volume_start_times=t)
-    g = slice_time.slice_generator(f, np.zeros(t.shape))
+    g = slice_time.slice_generator(f, np.zeros(Y.shape[1]))
     ff = slice_time.slice_time(f, g, interpolator=slice_time.sinc_interp)
     assert(np.allclose(np.asarray(ff), Y))
 
-def test_nointerp_sinc():
-    t = np.linspace(0,7,7)
-    s = np.linspace(0.2,0.8,5)
-    Y = np.multiply.outer(t, np.ones((5,4,5)))
+def test_linear():
+    t = np.linspace(0,7*2.3,7)
+    s = np.linspace(0.2,0.8,11)
+    Y = np.add.outer(np.linspace(0,7,7), np.random.standard_normal((11,4,5)))
     Y = Image(Y, CoordinateMap.identity(['t','z','y','x'], Y.shape))
     f = fromimage(Y, volume_start_times=t)
     g = slice_time.slice_generator(f, s)
     ff = slice_time.slice_time(f, g, bounds_error=False, fill_value=0.)
-    assert(np.allclose(np.asarray(ff)[3,4] + s[3], np.asarray(Y)[3,4]))
+    A = np.asarray(ff)[3,4] + s[4]/2.3
+    B = np.asarray(Y)[3,4]
+    assert(np.allclose(A, B))
 
 
